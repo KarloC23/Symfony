38a148edbae78907d558389203a92385852de24d
Karlo Cazar <karlo.cazar@gmail.com>
2019-04-03 17:00:17 +0200

    Day 3 done

--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -86,7 +86,7 @@ class UserAuthenticator extends AbstractFormLoginAuthenticator
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('homepage'));
     }
 
     protected function getLoginUrl()
