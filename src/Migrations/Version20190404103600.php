274397c4a022389da247c1275d28f02ad2b8e56a
Karlo Cazar <karlo.cazar@gmail.com>
2019-04-04 16:56:54 +0200

    Registration and log in almost done

--- src/Migrations/Version20190404103600.php
+++ src/Migrations/Version20190404103600.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190403123103 extends AbstractMigration
+final class Version20190404103600 extends AbstractMigration
 {
     public function getDescription() : string
     {
