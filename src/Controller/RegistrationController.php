37802e913bc5682eeca4718c3611543fc0695ee4
Karlo Cazar <karlo.cazar@gmail.com>
2019-04-04 11:02:06 +0200

    Registration in the making

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class RegistrationController
+{
+    /**
+     * @Route("/register", name="register")
+     */
+    public function registerAction()
+    {
+        //Create the form
+
+        //Check if the form is valid
+            //Then
+            //Hash the password
+            //Register the new user in the database
+
+        //render the register.html.twig
+    }
+
+
+}
